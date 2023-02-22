9f71d1c80f300a83e9028db66125a7498d9676b8
izabela <108488197+Bellss06@users.noreply.github.com>
2023-02-22 21:05:20 +0000

    Initial commit

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+class Apresentacao:    
+  def fala(self):
+    print("Olá eu sou a Luana o chat bot da loja Eliartes \n Muito prazer \n Para começar vou te fazer algumas perguntas:")
+
+
+a = Apresentacao()
+a.fala()
+
+class Pergunta1:
+  def question1(self):
+    print('Proxima pergunta:\n como você conheceu nossa loja? \n 1 - Pelas nossa redes sociais? \n 2 - passou na rua da loja? \n 3 - por outros meios?')
+
+
+b = Pergunta1()
+b.question1
+
+class Pergunta2:
+  def question2(self):
+    print('Proxima pergunta:\n Qual o serviço desejado? \n 1 - Aluguel de roupa ou itens de festas? \n 2 - Decoração de festa? \n 3 - Personalização de camisa?')
+
+c = Pergunta2()
+c.question2()
+  
---
+++ main.py
@@ -0,0 +1,68 @@
+from chatbot import *
+
+a.fala
+
+print(' ')
+
+print("Qual é o seu nome?")
+nome = input('>:')
+nome = nome.title()
+
+print(' ')
+
+
+while True:
+  if nome ==  'Izabela':
+    print(f'Esse é o nome da minha criadora muito prazer {nome}')
+    break
+
+  elif nome == 'Eliete':
+    print(f"Esse é o mesmo nome da dona da loja muito prazer {nome}")
+  else:
+    print("Muito prazer")
+    break
+
+
+
+
+print(' ')
+
+b.question1()
+leed = int(input('>:'))
+
+print(' ')
+
+if leed == 1:
+  print("Venha visitar nossa loja e São Jose da Lapa \n Rua Presidente Costa e Silva")
+elif leed == 2:
+  print("siga nosso instagram @tatatattata")
+else:
+  print("Venha visitar nossa loja e São Jose da Lapa \n Rua Presidente Costa e Silva siga nosso instagram @tatatattata ")
+
+print(' ')
+
+c.question2()
+serviço = int(input(">:"))
+
+print(' ')
+
+if serviço == 1:
+  print("Por favor Aguarde Eliete responder para ela citar o catalogo")
+
+
+
+elif serviço == 2:
+  print('Qual o tema da festa?')
+  festa = input('>:')
+  print("Por favor Aguarde Eliete responder")
+
+else:
+  print("qual tipo de personalização? \n 1- foto \n 2- logo \n 3- personalizado")
+  blusa = int('>:')
+  print("Por favor Aguarde Eliete responder")
+
+
+print(' ')
+print('espero que o atendimento tenha sido agradavel. Até mais')
+  
+  
